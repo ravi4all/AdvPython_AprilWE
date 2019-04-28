c751dd192880d066ee4c9e5c9008adb4d993b310
ravi4all <ravikanttyagi.2295@gmail.com>
2019-04-28 11:08:13 +0530

    Add files via upload

---
+++ GUI/calc.py
@@ -0,0 +1,107 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    # to setup the UI - to arrange all the widgets
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1260, 750)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(480, 20, 311, 61))
+        self.label.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(90, 200, 461, 51))
+        self.label_2.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(90, 310, 461, 51))
+        self.label_3.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";")
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(90, 420, 461, 51))
+        self.label_4.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";")
+        self.label_4.setObjectName("label_4")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(640, 200, 501, 61))
+        self.lineEdit.setStyleSheet("font: 18pt \"MS Shell Dlg 2\";")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(640, 310, 501, 61))
+        self.lineEdit_2.setStyleSheet("font: 18pt \"MS Shell Dlg 2\";")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(640, 420, 501, 61))
+        self.lineEdit_3.setStyleSheet("font: 18pt \"MS Shell Dlg 2\";")
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(220, 550, 111, 81))
+        self.pushButton.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";\n"
+"background-color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(440, 550, 111, 81))
+        self.pushButton_2.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";\n"
+"background-color: rgb(255, 255, 255);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(640, 550, 111, 81))
+        self.pushButton_3.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";\n"
+"background-color: rgb(255, 255, 255);")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(840, 550, 111, 81))
+        self.pushButton_4.setStyleSheet("font: 20pt \"MS Shell Dlg 2\";\n"
+"background-color: rgb(255, 255, 255);")
+        self.pushButton_4.setObjectName("pushButton_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1260, 31))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Basic Calculator"))
+        self.label_2.setText(_translate("MainWindow", "Enter First Number"))
+        self.label_3.setText(_translate("MainWindow", "Enter Second Number"))
+        self.label_4.setText(_translate("MainWindow", "Result"))
+        self.pushButton.setText(_translate("MainWindow", "+"))
+        self.pushButton_2.setText(_translate("MainWindow", "-"))
+        self.pushButton_3.setText(_translate("MainWindow", "/"))
+        self.pushButton_4.setText(_translate("MainWindow", "*"))
+
+        self.lineEdit_3.setReadOnly(True)
+        self.pushButton.clicked.connect(self.add)
+        self.pushButton_2.clicked.connect(self.sub)
+
+    def add(self):
+        f_num = int(self.lineEdit.text())
+        s_num = int(self.lineEdit_2.text())
+        result = f_num + s_num
+        self.lineEdit_3.setText(str(result))
+
+    def sub(self):
+        f_num = int(self.lineEdit.text())
+        s_num = int(self.lineEdit_2.text())
+        result = f_num - s_num
+        self.lineEdit_3.setText(str(result))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
